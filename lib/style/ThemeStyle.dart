9d952d3bfc2659d3d055977a6631f25744a277d1
Noor  Yasser <mrh61037@gmail.com>
2021-09-04 12:38:04 +0300

    add file style add main

---
+++ lib/style/ThemeStyle.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class ThemeStyle {
+
+  const ThemeStyle();
+
+  static const Color mainColor = const Color(0xFF393939);
+  static const Color secondColor = const Color(0xFF393939);
+  static const Color grey = const Color(0xFFE5E5E5);
+  static const Color background = const Color(0xFFf0f1f6);
+  static const Color titleColor = const Color(0xFF6C6C6C);
+  static const primaryGradient = const LinearGradient(
+    colors: const [ Color(0xFF393939), Color(0xff000000)],
+    stops: const [0.0, 1.0],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+  );
+}
