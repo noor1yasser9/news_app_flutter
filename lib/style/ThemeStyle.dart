476ef0898c06babf8d47405231acf7340837a873
Noor  Yasser <mrh61037@gmail.com>
2021-09-05 09:54:23 +0300

    update style

--- lib/style/ThemeStyle.dart
+++ lib/style/ThemeStyle.dart
@@ -11,6 +11,8 @@ class ThemeStyle {
   static const Color grey = const Color(0xFFE5E5E5);
   static const Color background = const Color(0xFFf0f1f6);
   static const Color titleColor = const Color(0xFF6C6C6C);
+  static const Color titleColor_100 = const Color(0x68383838);
+  static const Color dark = const Color(0xFF2F2F2F);
   static const primaryGradient = const LinearGradient(
     colors: const [ Color(0xFF393939), Color(0xff000000)],
     stops: const [0.0, 1.0],
